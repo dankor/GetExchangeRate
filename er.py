dfc234426eb5ea017f57c125aa95b93eed961dc6
Danylo Korostil <ted.korostiled@gmail.com>
2018-11-19 13:02:53 +0200

    Fixed KZ ssl error

--- er.py
+++ er.py
@@ -1,8 +1,11 @@
-import requests
-import sys
+import requests, sys, os
 from datetime import datetime,timedelta
 
+# Supress warnings
+requests.packages.urllib3.disable_warnings() 
+
 def get_rate(Country,CurrencyFrom,CurrencyTo,Date):
+	
 	if Country == 'UA' and CurrencyFrom == 'UAH':
 		r = requests.get("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange", params={'valcode': CurrencyTo, 'date': Date.replace('-', '')})
 		rate = r.text[r.text.find('<rate>')+6:r.text.find('</rate>')]
@@ -22,7 +25,8 @@ def get_rate(Country,CurrencyFrom,CurrencyTo,Date):
 		if r.text.find('"mid":') == -1:			
 			rate = get_rate(Country,CurrencyFrom,CurrencyTo,(datetime.strptime(Date, '%Y-%M-%d') + timedelta(days=-1)).strftime("%Y-%M-%d"))
 		else:
-			rate = r.text[r.text.find('"mid":')+6:r.text.find('}]}')]		
+			rate = r.text[r.text.find('"mid":')+6:r.text.find('}]}')]
+		
 
 	if Country == 'GE' and CurrencyFrom == 'GEL':
 		r = requests.post('https://www.nbg.gov.ge/index.php?m=582&lng=eng', data={'item': 'ALL', 'date_start': Date, 'date_end': Date, 'x':'42', 'y':'12', 'action':'search'})
@@ -33,11 +37,12 @@ def get_rate(Country,CurrencyFrom,CurrencyTo,Date):
 		rate = rate[:rate.find('</td>')].replace(' ','').replace('\n','').strip()
 
 	if Country == 'KZ' and CurrencyFrom == 'KZT' and CurrencyTo == 'USD':
-		r = requests.post('http://www.nationalbank.kz/?docid=362&switch=english', data={'docid': '362', 'eDate': Date[-2:]+ '/' + Date[5:7] + '/' + Date[:4], 'flag': '1', 'idval': '5','OK2': 'Show report', 'sDate': Date[-2:]+ '/' + Date[5:7] + '/' + Date[:4]})
+		r = requests.post('https://www.nationalbank.kz/?docid=362&switch=english', data={'docid': '362', 'eDate': Date[-2:]+ '/' + Date[5:7] + '/' + Date[:4], 'flag': '1', 'idval': '5','OK2': 'Show report', 'sDate': Date[-2:]+ '/' + Date[5:7] + '/' + Date[:4]}, verify=False)			
 		rate = r.text[r.text.find('US DOLLAR'):]
 		rate = rate[rate.find('<td align="center">')+19:]
-		rate = rate[rate.find('<td align="center">')+19:]
-		rate = rate[:rate.find('</td>')].replace(' ','').replace('\n','').strip()
+		rate = rate[rate.find('<td align="center">')+29:]
+		rate = rate[:rate.find('</td>')-9].replace(' ','').replace('\n','').strip()
+		
 
 	if Country == 'AZ' and CurrencyFrom == 'AZN':
 		r = requests.get('https://www.cbar.az/currencies/' + Date[-2:]+ '.' + Date[5:7] + '.' + Date[:4] + '.xml')
@@ -66,4 +71,5 @@ def get_rate(Country,CurrencyFrom,CurrencyTo,Date):
 
 
 print(get_rate(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4]))
+
 
