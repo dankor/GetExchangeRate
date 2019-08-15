fedd07382f5fc78e806959896dfad8195da6e527
Danylo Korostil <ted.korostiled@gmail.com>
2019-08-15 11:43:52 +0300

    Add fake headers

--- er.py
+++ er.py
@@ -4,19 +4,23 @@ from datetime import datetime,timedelta
 # Supress warnings
 requests.packages.urllib3.disable_warnings() 
 
+#Fake headers
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
+
+
 def get_rate(Country,CurrencyFrom,CurrencyTo,Date):
 	
 	if Country == 'UA' and CurrencyFrom == 'UAH':
-		r = requests.get("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange", params={'valcode': CurrencyTo, 'date': Date.replace('-', '')})
+		r = requests.get("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange", params={'valcode': CurrencyTo, 'date': Date.replace('-', '')},headers=headers)
 		rate = r.text[r.text.find('<rate>')+6:r.text.find('</rate>')]
 		
 	if Country == 'RU' and CurrencyFrom == 'RUB':
-		r = requests.get('http://www.cbr.ru/scripts/XML_daily.asp', params={'date_req': Date[-2:]+ '/' + Date[5:7] + '/' + Date[:4]})
+		r = requests.post('http://www.cbr.ru/scripts/XML_daily.asp', params={'date_req': Date[-2:]+ '/' + Date[5:7] + '/' + Date[:4]},headers=headers)
 		rate = r.text[r.text.find(CurrencyTo):]
 		rate = rate[rate.find('<Value>')+7:rate.find('</Value>')].replace(',','.')
 
 	if Country == 'BY' and CurrencyFrom == 'BYN':
-		r = requests.get('http://www.nbrb.by/API/ExRates/Rates', params={'onDate': Date,'Periodicity': '0'})
+		r = requests.get('http://www.nbrb.by/API/ExRates/Rates', params={'onDate': Date,'Periodicity': '0'},headers=headers)
 		rate = r.text[r.text.find(CurrencyTo):]
 		rate = rate[rate.find('"Cur_OfficialRate"')+19:rate.find('},{')]
 
@@ -29,7 +33,7 @@ def get_rate(Country,CurrencyFrom,CurrencyTo,Date):
 		
 
 	if Country == 'GE' and CurrencyFrom == 'GEL':
-		r = requests.post('https://www.nbg.gov.ge/index.php?m=582&lng=eng', data={'item': 'ALL', 'date_start': Date, 'date_end': Date, 'x':'42', 'y':'12', 'action':'search'})
+		r = requests.post('https://www.nbg.gov.ge/index.php?m=582&lng=eng', data={'item': 'ALL', 'date_start': Date, 'date_end': Date, 'x':'42', 'y':'12', 'action':'search'},headers=headers)
 		rate = r.text[r.text.find(CurrencyTo)+3:]
 		rate = rate[rate.find(CurrencyTo):]
 		rate = rate[rate.find('">')+2:]
@@ -37,7 +41,7 @@ def get_rate(Country,CurrencyFrom,CurrencyTo,Date):
 		rate = rate[:rate.find('</td>')].replace(' ','').replace('\n','').strip()
 
 	if Country == 'KZ' and CurrencyFrom == 'KZT' and CurrencyTo == 'USD':
-		r = requests.post('https://www.nationalbank.kz/?docid=362&switch=english', data={'docid': '362', 'eDate': Date[-2:]+ '/' + Date[5:7] + '/' + Date[:4], 'flag': '1', 'idval': '5','OK2': 'Show report', 'sDate': Date[-2:]+ '/' + Date[5:7] + '/' + Date[:4]}, verify=False)			
+		r = requests.post('https://www.nationalbank.kz/?docid=362&switch=english', data={'docid': '362', 'eDate': Date[-2:]+ '/' + Date[5:7] + '/' + Date[:4], 'flag': '1', 'idval': '5','OK2': 'Show report', 'sDate': Date[-2:]+ '/' + Date[5:7] + '/' + Date[:4]}, verify=False,headers=headers)			
 		rate = r.text[r.text.find('US DOLLAR'):]
 		rate = rate[rate.find('<td align="center">')+19:]
 		rate = rate[rate.find('<td align="center">')+29:]
@@ -50,17 +54,17 @@ def get_rate(Country,CurrencyFrom,CurrencyTo,Date):
 		rate = rate[rate.find('<Value>')+7:rate.find('</Value>')]
 
 	if Country == 'LT' and CurrencyFrom == 'EUR':
-		r = requests.get('http://old.lb.lt/webservices/FxRates/FxRates.asmx/getFxRates', params={'tp': 'EU', 'dt': Date})
+		r = requests.get('http://old.lb.lt/webservices/FxRates/FxRates.asmx/getFxRates', params={'tp': 'EU', 'dt': Date},headers=headers)
 		rate = r.text[r.text.find(CurrencyTo):]
 		rate = rate[rate.find('<Amt>')+5:rate.find('</Amt>')]
 
 	if Country == 'LV' and CurrencyFrom == 'EUR':
-		r = requests.get('https://www.bank.lv/vk/ecb.xml', params={'date':Date.replace('-', '')})
+		r = requests.get('https://www.bank.lv/vk/ecb.xml', params={'date':Date.replace('-', '')},headers=headers)
 		rate = r.text[r.text.find(CurrencyTo):]
 		rate = rate[rate.find('<Rate>')+6:rate.find('</Rate>')]
 
 	if Country == 'EE' and CurrencyFrom == 'EUR':
-		r = requests.get('https://www.eestipank.ee/en/historical-exchange-rates', params={'is_ajax':'true', 'chart_start_at':Date[-2:]+ '.' + Date[5:7] + '.' + Date[:4], 'chart_end_at':Date[-2:]+ '.' + Date[5:7] + '.' + Date[:4], 'chart_step':'day', 'currency_code': CurrencyTo})
+		r = requests.get('https://www.eestipank.ee/en/historical-exchange-rates', params={'is_ajax':'true', 'chart_start_at':Date[-2:]+ '.' + Date[5:7] + '.' + Date[:4], 'chart_end_at':Date[-2:]+ '.' + Date[5:7] + '.' + Date[:4], 'chart_step':'day', 'currency_code': CurrencyTo},headers=headers)
 		rate = r.text		
 		if rate.find('"1 EUR = ') == -1:			
 			rate = get_rate(Country,CurrencyFrom,CurrencyTo,(datetime.strptime(Date, '%Y-%M-%d') + timedelta(days=-1)).strftime("%Y-%M-%d"))
