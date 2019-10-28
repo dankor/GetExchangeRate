a2a4acaadc33c87ea76a3b87395070fa597c62db
Danylo Korostil <ted.korostiled@gmail.com>
2019-10-28 09:56:05 +0200

    Added the UZS currency

--- er.py
+++ er.py
@@ -5,8 +5,7 @@ from datetime import datetime,timedelta
 requests.packages.urllib3.disable_warnings() 
 
 #Fake headers
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
-
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36','Connection': 'keep-alive'}
 
 def get_rate(Country,CurrencyFrom,CurrencyTo,Date):
 	
@@ -29,8 +28,7 @@ def get_rate(Country,CurrencyFrom,CurrencyTo,Date):
 		if r.text.find('"mid":') == -1:			
 			rate = get_rate(Country,CurrencyFrom,CurrencyTo,(datetime.strptime(Date, '%Y-%M-%d') + timedelta(days=-1)).strftime("%Y-%M-%d"))
 		else:
-			rate = r.text[r.text.find('"mid":')+6:r.text.find('}]}')]
-		
+			rate = r.text[r.text.find('"mid":')+6:r.text.find('}]}')]		
 
 	if Country == 'GE' and CurrencyFrom == 'GEL':
 		r = requests.post('https://www.nbg.gov.ge/index.php?m=582&lng=eng', data={'item': 'ALL', 'date_start': Date, 'date_end': Date, 'x':'42', 'y':'12', 'action':'search'},headers=headers)
@@ -45,8 +43,7 @@ def get_rate(Country,CurrencyFrom,CurrencyTo,Date):
 		rate = r.text[r.text.find('US DOLLAR'):]
 		rate = rate[rate.find('<td align="center">')+19:]
 		rate = rate[rate.find('<td align="center">')+29:]
-		rate = rate[:rate.find('</td>')-9].replace(' ','').replace('\n','').strip()
-		
+		rate = rate[:rate.find('</td>')-9].replace(' ','').replace('\n','').strip()		
 
 	if Country == 'AZ' and CurrencyFrom == 'AZN':
 		r = requests.get('https://www.cbar.az/currencies/' + Date[-2:]+ '.' + Date[5:7] + '.' + Date[:4] + '.xml')
@@ -71,6 +68,21 @@ def get_rate(Country,CurrencyFrom,CurrencyTo,Date):
 		else:
 			rate = rate[rate.find('"1 EUR = ')+9:]
 			rate = rate[:rate.find(' ' + CurrencyTo)]
+
+	if Country == 'UZ' and CurrencyFrom == 'UZS':
+		headers_get = headers.copy()
+		headers_get.update({'BX-ACTION-TYPE': 'get_dynamic', 'BX-CACHE-MODE': 'HTMLCACHE'})
+		r = requests.get('https://nbu.uz/exchange-rates/?bxrand=int(datetime.now().timestamp())',headers=headers_get)
+		headers.update({'Cookie':'PHPSESSID=' + str(r.cookies.get_dict()['PHPSESSID'])})
+		headers.update({'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'})
+		headers.update({'X-Requested-With': 'XMLHttpRequest'})
+		sessid = r.text[r.text.find('bitrix_sessid')+16:]
+		sessid = sessid[:sessid.find('}')-1]
+		rate = requests.post('https://nbu.uz/api/exchange_archive/', data={'date':Date[-2:]+ '.' + Date[5:7] + '.' + Date[:4],'sessid': sessid},headers=headers).text
+		rate = rate[rate.find(CurrencyTo):]
+		rate = rate[rate.find('<td>')+4:]
+		rate = rate[:rate.find('</td>')]
+
 	return(rate)
 
 
